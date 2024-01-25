e4e908b495571d1c112c84a3bf30577d9bf9c7e3
906009 <110044475+906009@users.noreply.github.com>
2024-01-26 02:46:32 +0500

    Add files via upload
    Проект за 31.03.2022

--- main.py
+++ main.py
@@ -21,8 +21,45 @@ class MikroLiteApp(MDApp):
             prt = int(self.root.get_screen('Authorization').ids.portv.text)
             usr = self.root.get_screen('Authorization').ids.user.text
             pwr = self.root.get_screen('Authorization').ids.passwordv.text
-            connection = routeros_api.RouterOsApiPool(host, username=usr,password=pwr, port=prt,use_ssl=False, ssl_verify=False, ssl_verify_hostname=False,ssl_context=None, plaintext_login=True)
-            api = connection.get_api()
+            self.connection = routeros_api.RouterOsApiPool(host, username=usr, password=pwr, port=prt, use_ssl=False,
+                                                       ssl_verify=False, ssl_verify_hostname=False, ssl_context=None,
+                                                       plaintext_login=True)
+            self.api = self.connection.get_api()
+
+            return self.api
+
+
+
+    def resour(self):
+        #Опора
+        res = self.api.get_resource('/system/resource')
+        #Версия ОС
+        index = str(res.get()).find("version")
+        a = index + 14
+        while a < len(str(res.get())):
+            if str(res.get())[a] == "'":
+                break
+            else:
+                a = a + 1
+        self.root.get_screen('MainMenu').ids.resource.text = 'Версия RouterOS:\n'+(str(res.get())[index+11:a])
+        #Получение загрузки CPU
+        index = str(res.get()).find("cpu-load")
+        a = index + 14
+        while a < len(str(res.get())):
+            if str(res.get())[a] == "'":
+                break
+            else:
+                a = a + 1
+        self.root.get_screen('MainMenu').ids.loadcpu.text = 'Загрузка CPU:\n'+(str(res.get())[index + 12:a])+'%'
+        #Получение модели
+        index = str(res.get()).find("board-name")
+        a = index + 14
+        while a < len(str(res.get())):
+            if str(res.get())[a] == "'":
+                break
+            else:
+                a = a + 1
+        self.root.get_screen('MainMenu').ids.model.text = 'Модель:\n'+(str(res.get())[index + 14:a])
     def build(self):
         sm = Manager()
         sm.add_widget(Authorization(name='Authorization'))
